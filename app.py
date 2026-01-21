05ba7a0267debde347d00db032ebe68f46b5ddf5
Bhabagrahi Behera <bhabagrahibehera@Bhabagrahis-MacBook-Air.local>
2026-01-21 23:44:12 +0530

    include streaming 1

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def text_gen_stream():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=True)
